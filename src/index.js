c67e310796cc3936c82ad5d6641675bf154e7e91
Nikita Nikitin <nickitin_2010@mail.ru>
2023-12-18 19:16:06 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { VK } from 'vk-io';
+import 'dotenv/config'
+import TelegramBot from 'node-telegram-bot-api'
+const { env: { VK_TOKEN, VK_IDS, T_TOKEN, T_CHAT_ID } } = process
+
+const vk = new VK({
+    token: VK_TOKEN
+});
+const POLLING_SETTINGS = {
+  interval: 300,
+  autoStart: true
+}
+const bot = new TelegramBot(T_TOKEN, {polling: POLLING_SETTINGS});
+const onlined = new Map();
+let chatId = null
+let stopSpy = false
+
+bot.onText(/\/start/, async (msg) => {
+  const { chat: { id } } = msg
+  chatId = id
+  stopSpy = false
+  onlined.clear()
+
+  bot.sendMessage(id, 'Вы подписались на рассылку')
+})
+
+bot.onText(/\/stop/, () => {
+  chatId = null
+  stopSpy = true
+  onlined.clear()
+
+  bot.sendMessage(id, 'Вы отписались')
+})
+
+setInterval(async () => {
+  const chId = chatId || T_CHAT_ID
+  if(!chId || stopSpy) return
+
+  for (const vkId of VK_IDS.split(',')){
+    try {
+      const res = await vk.api.users.get({
+        user_ids: vkId,
+        fields: 'online'
+      })
+      const isOnline = res[0].online === 1;
+      const msg = isOnline ? 'Онлайн' : 'Оффлайн';
+      if (isOnline && !onlined.has(vkId)) {
+        bot.sendMessage(chId, `${vkId}: ${msg}`);
+        onlined.set(vkId, Date.now())
+
+        setTimeout(() => {
+          onlined.clear()
+        }, 60 * 30 * 1000)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}, 5000);
